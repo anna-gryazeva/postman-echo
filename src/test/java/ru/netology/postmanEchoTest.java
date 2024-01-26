827a0b395ace100e0fbc254adf2471dfb60fdc62
Anna Gryazeva <anna_gryazeva@mail.ru>
2024-01-27 00:40:26 +0300

    Initial commit

---
+++ src/test/java/ru/netology/postmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class postmanEchoTest {
+
+    @Test
+    void returnData() {
+        var text = "some data";
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo(text));
+
+    }
+}
